a89324daaa30f0a65085d45eb39fa293e5bb9ebf
sgk20071988 <56190073+sgk20071988@users.noreply.github.com>
2020-02-15 19:01:43 +0700

    Revert "Revert "обучение""
    This reverts commit c41afa921f90cabbe5250a934178763ba03de519.

---
+++ шарик1/Assets/скрипты/монетка.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class монетка : MonoBehaviour
+{
+    public bool pro;
+    static public float money;
+    public Text TextMon;
+    public int Cena;
+
+
+    void OnTriggerEnter(Collider col)
+    { if (col.gameObject.CompareTag("pickup") && pro == true)
+        {
+            money = money + Cena;
+            Destroy(col.gameObject);
+            TextMon.text = money.ToString();
+            pro = false;
+        }
+        else pro = true;
+    }
+}
---
+++ шарик1/Assets/скрипты/солнце.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class солнце : MonoBehaviour {
+	float g = 0f ;
+	float h = 0.002f ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		this.transform.rotation = Quaternion.Euler (g, 0, 0);	
+	
+		g = g + h;
+
+		if (Input.GetKey ("r")) {
+			g = g + 1;
+		}
+	}
+}
