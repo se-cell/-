2ff189c4f1cab82a4453844f7e73808c6fa359ef
sgk20071988 <56190073+sgk20071988@users.noreply.github.com>
2020-02-15 19:45:24 +0700

    Обучение

---
+++ шарик1/Assets/скрипты/голова.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class голова : MonoBehaviour {
+	public float x = 0;
+	public float y = 0;
+	public float w = 90;
+	public float s = -90;
+	public float a = 60;
+	public float d = -60;
+	float n = 0;
+	private Quaternion Q;
+	private Quaternion M;
+	private float x1 = 0;
+	private float y1 = 0;
+	void Start () 
+	{
+		Rigidbody u = GetComponent<Rigidbody> ();
+		if (u) 
+		{
+			u.freezeRotation = true;
+
+		}
+		Q = transform.localRotation;
+		M = transform.localRotation ;
+	}
+	void Update ()
+
+	{
+		if (Input.GetKey ("f"))		{
+			x1 += Input.GetAxis ("Mouse X");
+			y1 += Input.GetAxis ("Mouse Y");
+
+			x1 = x1 % 360;
+			y1 = y1 % 360;
+
+			x1 = Mathf.Clamp (x1, s, w);
+			y1 = Mathf.Clamp (y1, d, a);
+
+			Quaternion Q3 = Quaternion.AngleAxis (x1, Vector3.up);
+			Quaternion Q2 = Quaternion.AngleAxis (y1, Vector3.left);
+			//
+			transform.localRotation = Q2 * Q3 * Q;
+		}
+		if (Input.GetKeyUp ("f")) {
+			this.transform.localRotation = M * Q * M;
+		}
+
+	}
+}
---
+++ шарик1/Assets/скрипты/пушка3.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class пушка3 : MonoBehaviour {
+
+	public GameObject yu;
+	public GameObject yu2;
+	private GameObject t2;
+	private GameObject t3;
+	public Transform v;
+	public Transform v2;
+	public float n = 1f;
+
+	void Update () 
+	{
+		if (Input.GetButtonDown("Fire2"))
+		{
+			spop ();
+		}
+
+
+	}
+	void spop () 
+	{
+		t2 = Instantiate(yu, v.position, v.rotation);
+		t3 = Instantiate(yu2, v2.position, v2.rotation);
+		t2.GetComponent<Rigidbody>().AddForce (t2.transform.forward * n, ForceMode.Impulse);
+		Destroy (t2, 3f);
+		Destroy (t3, 4f);
+	}
+}
